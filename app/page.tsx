c760ff81e773f1c690d0129e9867d218a1639919
pilcik_patrik <pilcikpatrik@seznam.cz>
2023-10-27 11:40:40 +0200

    aktualizace portfolia

--- app/page.tsx
+++ app/page.tsx
@@ -2,69 +2,67 @@ import { Github, LinkedIn, Twitter } from "@/components/shared/icons";
 import { Dribbble, Instagram } from "lucide-react";
 
 const Home = () => (
-  <section className='w-full flex-center flex-col pb-10 pt-10'>
-    <h1 className='head_text text-center'>
+  <section className="w-full flex-center flex-col pb-10 pt-10">
+    <h1 className="head_text text-center">
       Patrik
-      <br className='max-md:hidden' />
-      <span className='bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent text-center'>Web developer</span>
+      <br className="max-md:hidden" />
+      <span className="bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent text-center">
+        Web developer
+      </span>
     </h1>
-    <p className='desc text-center'>
-      Specializuji se na vývoj webových stránek
+    <p className="desc text-center">
+      Specializuji se na vývoj webových stránek a grafiky
     </p>
     <div className="mx-auto mt-10 flex max-w-fit space-x-4">
-        <a
-          href=""
-          className="rounded-full border border-black bg-black px-5 py-2 text-sm text-white shadow-lg transition-all hover:bg-white hover:text-black"
-        >
-          Zobrazit CV
-        </a>
-        <a
-          className="flex items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 shadow-lg transition-all hover:border-gray-800"
-          href="https://github.com/pilcikpatrik"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Github className="h-5 w-5 text-black" />
-          <p className="text-sm">GitHub</p>
-        </a>
-      </div>
-      <div className="mt-10 relative">
-        <img 
-          src="images/profile.jpg" 
-          alt="profile" 
-          className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[300px] lg:h-[300px] rounded-full object-cover"  
-        />
-      </div>
-      <div className=" mt-5 flex items-center justify-center space-x-4">
-          <a    
-            href="https://www.linkedin.com/in/patrik-pil%C4%8D%C3%ADk-6a7166239/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <LinkedIn className="h-10 w-10 hover:border-gray-800 p-2 rounded-lg border border-gray-300 bg-white shadow-lg transition-all"/>
-          </a>
-          <a    
-            href="https://dribbble.com/pilcikpatrik"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Dribbble className="h-10 w-10 hover:border-gray-800 p-2 rounded-lg border border-gray-300 bg-white shadow-lg transition-all"/>
-          </a>
-          <a    
-            href="https://www.instagram.com/patriik074/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Instagram className="h-10 w-10 hover:border-gray-800 p-2 rounded-lg border border-gray-300 bg-white shadow-lg transition-all" />
-          </a>
-          <a    
-            href="https://twitter.com/PPilcik"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Twitter className="h-10 w-10 hover:border-gray-800 p-2 rounded-lg border border-gray-300 bg-white shadow-lg transition-all"/>
-          </a>
-        </div>
+      <a
+        href=""
+        className="rounded-full border border-black bg-black px-5 py-2 text-sm text-white shadow-lg transition-all hover:bg-white hover:text-black"
+      >
+        Zobrazit CV
+      </a>
+      <a
+        className="flex items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 shadow-lg transition-all hover:border-gray-800"
+        href="https://github.com/pilcikpatrik"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Github className="h-5 w-5 text-black" />
+        <p className="text-sm">GitHub</p>
+      </a>
+    </div>
+    <div className="mt-10 relative">
+      <img
+        src="images/profile.jpg"
+        alt="profile"
+        className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[300px] lg:h-[300px] rounded-full object-cover"
+      />
+    </div>
+    <div className=" mt-5 flex items-center justify-center space-x-4">
+      <a
+        href="https://www.linkedin.com/in/patrik-pil%C4%8D%C3%ADk-6a7166239/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedIn className="h-10 w-10 hover:border-gray-800 p-2 rounded-lg border border-gray-300 bg-white shadow-lg transition-all" />
+      </a>
+      <a
+        href="https://dribbble.com/pilcikpatrik"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Dribbble className="h-10 w-10 hover:border-gray-800 p-2 rounded-lg border border-gray-300 bg-white shadow-lg transition-all" />
+      </a>
+      <a
+        href="https://www.instagram.com/patriik074/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Instagram className="h-10 w-10 hover:border-gray-800 p-2 rounded-lg border border-gray-300 bg-white shadow-lg transition-all" />
+      </a>
+      <a href="https://twitter.com/PPilcik" target="_blank" rel="noreferrer">
+        <Twitter className="h-10 w-10 hover:border-gray-800 p-2 rounded-lg border border-gray-300 bg-white shadow-lg transition-all" />
+      </a>
+    </div>
   </section>
 );
 
--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useState } from "react";
 
 interface ImageSliderProps {
   images: string[];
@@ -12,24 +12,30 @@ const ImageSlider: FC<ImageSliderProps> = ({ images }) => {
   };
 
   return (
-    <div className="relative w-full h-[150px] md:h-[400px] lg:h-[420px] overflow-hidden">
-      <div className="absolute top-0 left-0 w-full h-full">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Slider image ${index}`}
-            className={`absolute inset-0 object-contain w-full h-full transform scale-105 transition-all duration-500 ease-in-out ${currentImage === index ? 'opacity-100 scale-100' : 'opacity-0'}`}
-          />
-        ))}
+    <div>
+      <div className="relative w-full h-[150px] md:h-[400px] lg:h-[420px] overflow-hidden">
+        <div className="top-0 left-0 w-full h-full">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Slider image ${index}`}
+              className={`absolute inset-0 object-contain w-full h-full transform scale-105 transition-all duration-500 ease-in-out ${
+                currentImage === index ? "opacity-100 scale-100" : "opacity-0"
+              }`}
+            />
+          ))}
+        </div>
       </div>
-      <div className="absolute bottom-1 w-full h-10 md:h-14 lg:h-20 flex justify-center gap-x-1 overflow-x-auto">
+      <div className="w-full h-10 md:h-14 lg:h-20 flex justify-center gap-x-1 overflow-x-auto">
         {images.map((image, index) => (
           <img
             key={index}
             src={image}
             alt={`Thumbnail ${index}`}
-            className={`w-[80px] md:w-[112px] lg:w-[160px] object-cover cursor-pointer border border-gray-300 bg-white shadow-lg ${currentImage === index ? 'border-gray-300' : 'border-transparent'}`}
+            className={`w-[80px] md:w-[112px] lg:w-[160px] object-cover cursor-pointer border border-gray-300 bg-white shadow-lg ${
+              currentImage === index ? "border-gray-300" : "border-transparent"
+            }`}
             onClick={() => selectImage(index)}
           />
         ))}
@@ -39,6 +45,3 @@ const ImageSlider: FC<ImageSliderProps> = ({ images }) => {
 };
 
 export default ImageSlider;
-
-
-
